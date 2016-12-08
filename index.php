769580051d272f34827786367c4d0e485b9c2b53
bastVi <bastien.viglianti@gmail.com>
2016-12-08 16:38:26 +0100

    image 2 (à modifier si envie, on s'en branle)

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
 <body>
     <header>
 	<div class="link-container">
-		<a href="festival.html">Le festival</a>
-		<a href="courts-metrages.html">Les courts metrages</a>
+		<a href="#festival">Le festival</a>
+		<a href="#CM">Les courts metrages</a>
 	</div>
 	<div>
-		<a href="index.php">
+		<a href="#acceuil">
 			<img src="img/logo.png" alt="Logo du festival" id="logo">
 		</a>
 	</div>
 	<div class="link-container">
 		<a href="inscription.php">Inscription</a>
-		<a href="contact.html">Accès</a>
+		<a href="#acces">Accès</a>
 	</div>
     </header>
     
@@ -27,17 +27,26 @@
             <p> Bienvenue </p>
         </div>
         <div id="compteur">
-            <p> Heure de s'acheter une montre </p>
+            <p> Le compteur ici </p>
         </div>
     </section>
     
     <section id="festival">
-        
-        
+        <div id="prog">
+            <p> Programme du festival</p>
+        </div>
+       
     </section>
     
     <section id="CM">
         
+        
+    </section>
+    
+    <section id="acces">
+        <div id="map">
+            
+        </div>
         
     </section>
 
