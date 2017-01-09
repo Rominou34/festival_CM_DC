944f1e323fe0b6e56a2496754953c3794ff97571
Romain Arnaud <arnaud.romain34@gmail.com>
2017-01-09 12:57:32 +0100

    Amelioration responsive design

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include('formulaires.php');
 <head>
 	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<title>Festival du court-metrage</title>
 </head>
